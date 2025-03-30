0f8590974836c3fd42f47c4e18875dcc36b5ea2d
LazyManish <mbhattarai900@gmail.com>
2025-03-30 12:54:21 +0545

    addded slug field

--- main_news/models.py
+++ main_news/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your models here.
 
@@ -7,8 +9,34 @@ from django.contrib.auth.models import User
 class Category(models.Model):
 
     name = models.CharField(max_length=50, null=False)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+    def get_url(self):
+        return reverse('news-category', args=[self.slug])
+
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+
+            self.slug = slugify(self.name)
+
+            original_slug = self.slug
+
+            count = 1
+
+            while Post.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug} - {count}"
+
+                count += 1
+        
+        super().save(*args, **kwargs)
+    
+    
+
 
     def __str__(self):
         return self.name
@@ -19,17 +47,31 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=100, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='post_categ', null=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     image = models.ImageField(upload_to='news/')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+
+            self.slug = slugify(self.title)
+
+            original_slug = self.slug
+
+            count = 1
+
+            while Post.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug} - {count}"
+
+                count += 1
+        
+        super().save(*args, **kwargs)
 
 
 
     def __str__(self):
         return f"{self.author.username}'s Post."
     
-
-
-
-
--- main_news/urls.py
+++ main_news/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('news/',views.main_page, name='news'),
+    path('news/', views.main_page,name='news'),
+    path('news/<slug:category_slug>/',views.main_page, name='news-category'),
     path('article/', views.article, name='article'),
 
 ]
